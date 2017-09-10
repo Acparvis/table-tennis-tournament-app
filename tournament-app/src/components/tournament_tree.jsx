591c9daf9fa2f36160a684aae53f82ef66eb5c06
Acparvis <acparvis@googlemail.com>
2017-09-10 14:38:28 +0100

    Began tournament Array
    Experimented with randomising the array pulled from state, haven't got
    it working yet, however the Tournament Render area is now displaying
    players using a .get() immutable request.

--- tournament-app/src/components/tournament_tree.jsx
+++ tournament-app/src/components/tournament_tree.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
 
-export default ({ player, onClick, onChange, makeTree }) => (
+export default ({ player }) => (
 	<div>
 		<h1>Tournament Tree</h1>
+		<ul>
+			{ player.map(( player, i ) => (
+				//This loop iterates over each palyer in the list and creates a list item with the value.
+				<li>
+					{ player.get(["players"], player.get('value')) }
+				</li>
+			)) }
+
+		</ul>
 	</div>
 );
--- tournament-app/src/containers/tournament_tree.js
+++ tournament-app/src/containers/tournament_tree.js
@@ -1,9 +1,7 @@
 import { connect } from "react-redux";
 
 import {
-	updateText,
-	updateList,
-	updateId,
+
 } from "../data/actions/state";
 
 import TournamentTree from "../components/tournament_tree";
@@ -11,6 +9,21 @@ import TournamentTree from "../components/tournament_tree";
 
 
 const mapStateToProps = state => {
+	let playerList = state.get("players");
+	let test = [1, 2, 3, 4, 5];
+
+	// this function randomises the order of the players in the list
+	function shuffleArray(a) { // Fisher-Yates shuffle
+    var i = a.length, t, j;
+    a = a.slice();
+    while (--i) t = a[i], a[i] = a[j = ~~(Math.random() * (i+1))], a[j] = t;
+    return a;
+	}
+	const newPlayerList = shuffleArray(playerList);
+	console.log(newPlayerList);
+	return {
+		player: newPlayerList,
+	}
 
 };
 
