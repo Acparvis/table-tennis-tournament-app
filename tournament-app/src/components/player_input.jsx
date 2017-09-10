6d22d6d6ec701a844d2971c7802ffb03d13ff164
Acparvis <acparvis@googlemail.com>
2017-09-10 21:30:28 +0100

    Updated Input box
    Input box now submits on Enter key, also both the Enter button  and
    enter key press only submit when there is information in the box.

--- tournament-app/src/components/player_input.jsx
+++ tournament-app/src/components/player_input.jsx
@@ -4,10 +4,19 @@ import React from "react";
 //@todo set input field to reset after Enter button is pressed.
 export default ({ textChange, value, player, playerSubmit, currentId, increaseId}) => (
 	<div>
-	        <input onChange={ (e) => textChange(e.target.value) } />
+	        <input onChange={ (e) => textChange(e.target.value) }
+				onKeyPress={ (e) => {
+					if(e.key === "Enter" && e.target.value.length > 0){
+						playerSubmit(value, currentId);
+						increaseId(currentId);
+					}
+				} }
+			/>
 			<button onClick={ () => {
-				playerSubmit(value, currentId);
-				increaseId(currentId);
+					if ( value.length > 0) {
+					playerSubmit(value, currentId);
+					increaseId(currentId);
+				}
 			} } >Enter</button>
 			<p>{ value }</p>
     </div>
